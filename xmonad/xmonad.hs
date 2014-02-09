56502f413abfa82f1bb0b208b79b6c14b1eebab9
jmcloughlin <jmcloughlin.lists@gmail.com>
2014-02-09 12:30:57 -0800

    Initial laptop install config.

---
+++ xmonad/xmonad.hs
@@ -0,0 +1,18 @@
+import XMonad
+import XMonad.Hooks.DynamicLog
+import XMonad.Hooks.ManageDocks
+import XMonad.Util.Run(spawnPipe)
+import XMonad.Util.EZConfig(additionalKeys)
+import System.IO
+
+main = do
+    xmproc <- spawnPipe "/usr/bin/xmobar /home/jmcl/.xmonad/.xmobarrc"
+    xmonad $ defaultConfig
+        { manageHook = manageDocks <+> manageHook defaultConfig
+        , layoutHook = avoidStruts  $  layoutHook defaultConfig
+	, modMask = mod4Mask
+	, logHook = dynamicLogWithPP xmobarPP
+                        { ppOutput = hPutStrLn xmproc
+                        , ppTitle = xmobarColor "green" "" . shorten 50
+                        }
+        }
