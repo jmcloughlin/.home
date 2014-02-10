99979163bd352527b924c3f45f6192143c4e7d31
jmcloughlin <jmcloughlin.lists@gmail.com>
2014-02-10 10:42:24 -0800

    Font size change plus xmobar config.

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -6,7 +6,7 @@ import XMonad.Util.EZConfig(additionalKeys)
 import System.IO
 
 main = do
-    xmproc <- spawnPipe "/usr/bin/xmobar /home/jmcl/.xmonad/.xmobarrc"
+    xmproc <- spawnPipe "/usr/bin/xmobar /home/jmcl/.home/xmonad/.xmobarrc"
     xmonad $ defaultConfig
         { manageHook = manageDocks <+> manageHook defaultConfig
         , layoutHook = avoidStruts  $  layoutHook defaultConfig
